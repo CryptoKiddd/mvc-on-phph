ab17c811e6200469602a0ed61fac51a208232ac1
cryptokiddd <avaliani1232@gmail.com>
2024-07-03 05:30:27 +0400

    router:routes array

--- core/Router.php
+++ core/Router.php
@@ -1,10 +1,13 @@
 <?php
+
+namespace app\core;
 class Router {
+protected array $routes = [];
 
 
+public function get($path,$callback){
+    $this->routes['get'][$path] = $callback
 
-public function __construct()
-{
 }
 
 
