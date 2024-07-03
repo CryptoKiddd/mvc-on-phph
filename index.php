4266ea137240ce521d9bde0480b3d7cf382a4351
cryptokiddd <avaliani1232@gmail.com>
2024-07-03 05:56:15 +0400

    get path method

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
-require_once
+require_once __DIR__.'/vendor/autoload.php';
+use app\core\Application;
 
 $app = new Application();
 
 $app->router->get('/', function(){
-    return 'hello world';
+  echo 'hello world';
 });
+
+$app->run();
